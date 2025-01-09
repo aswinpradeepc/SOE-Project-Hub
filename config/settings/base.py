e8193b221b7656421b7d11e09dceced2f7a94b5a
aswinpradeepc <aswinpradeepc@gmail.com>
2025-01-09 23:32:35 +0530

    rename settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -12,21 +12,19 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '127.0.0.1:8000']
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zzu6=lywjyx7db%4%4id(@1eu4*_^(19&xdm@6hhkh4g(_sfw*'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '127.0.0.1:8000']
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 # Application definition
@@ -57,7 +55,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'config.urls'
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 TEMPLATES = [
     {
