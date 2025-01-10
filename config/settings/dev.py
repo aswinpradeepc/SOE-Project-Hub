b4f5ad1b33aa5c241e356b0206481c91390144dc
aswinpradeepc <aswinpradeepc@gmail.com>
2025-01-11 04:14:03 +0530

    edit settings

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -1,3 +1,5 @@
 from .base import *
 
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '127.0.0.1:8000']
+
 DEBUG=True
--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,3 +1,5 @@
 from .base import *
 
 DEBUG=False
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '127.0.0.1:8000', '3.89.114.159']
